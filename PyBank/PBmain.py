cbeaf5122217adaa24e59982851a5f10d48690d1
Katelyn Burke <katelyn.burke2@gmail.com>
2019-03-07 22:14:33 -0600

    calculated average

--- PyBank/PBmain.py
+++ PyBank/PBmain.py
@@ -5,30 +5,67 @@ import csv
 
 # set the path for the file
 csvpath = os.path.join("budget_data.csv")
-    
 
+# opened the csv file
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     print(csvreader)
 
     # read the header row of the file
     csv_header = next(csvreader)
-    # print the header
-    print(f"CSV Header: {csv_header}")
+
+    # to print header
+    # print(f"CSV Header: {csv_header}")
 
     # total begins at zero
     total = 0
-    #reader = csv.reader(bankData)
+    monthsProfits = []
+    # reader = csv.reader(bankData)
     for row in csvreader:
         total += int(row[1])
+        # monthsProfits.append(int(row[1]))
+        monthProfit = [row[0], int(row[1])]
+        monthsProfits.append(monthProfit)
 
-    print('total')
-    print(total)
+    # average of changes of profits/losses
+    monthDifferences = []
+    for idx, row in enumerate(monthsProfits):
+        # print(row, idx) 
+        # 867884 , 0
+        #If the index plus 1 is less than the number of months 
+        if (idx + 1) < len(monthsProfits):
+            current = monthsProfits[idx][1]
+            next = monthsProfits[idx + 1][1]
+            monthDifferences.append(int(next - current))
+
+    # gives a list of all of the differences (changes) between the months
+    # now, find average by adding all of the numbers in the list
+    # and dividing by the length of the list
+    avgMonthlyDifferencesTotal = 0
+    for monthDifference in monthDifferences:
+        avgMonthlyDifferencesTotal += monthDifference
+
+
+
+    print("Financial Analysis")
+
+    print("-------------------------")
 
     # count the rows of data to see how many months
     # subtract the header row
-    count = len(open(csvpath).readlines( ))
-    print("Months:", str(count - 1))
+    count = len(open(csvpath).readlines())
+    print("Total Months:", str(count - 1))
+
+    print(f"Total: ${str(total)}")
+
+    avgChange = avgMonthlyDifferencesTotal / len(monthDifferences)
+    print(f"Average Change: ${round(avgChange, 2)}")
+    
+    # calculate the average of the changes in profit/losses
+    #bankAverage = (total / count)
+    #print(bankAverage)
+
+
 
 
 
