45b3b25cf027fd17b1326bf458606d5d7e77a6c9
Katelyn Burke <katelyn.burke2@gmail.com>
2019-03-09 07:25:45 -0600

    found greatest inc and dec in profits

--- PyBank/PBmain.py
+++ PyBank/PBmain.py
@@ -9,7 +9,7 @@ csvpath = os.path.join("budget_data.csv")
 # opened the csv file
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    print(csvreader)
+    # print(csvreader)
 
     # read the header row of the file
     csv_header = next(csvreader)
@@ -19,6 +19,7 @@ with open(csvpath, newline="") as csvfile:
 
     # total begins at zero
     total = 0
+    # empty list for the profits each month
     monthsProfits = []
     # reader = csv.reader(bankData)
     for row in csvreader:
@@ -34,17 +35,24 @@ with open(csvpath, newline="") as csvfile:
         # 867884 , 0
         #If the index plus 1 is less than the number of months 
         if (idx + 1) < len(monthsProfits):
-            current = monthsProfits[idx][1]
-            next = monthsProfits[idx + 1][1]
-            monthDifferences.append(int(next - current))
+            # 11231
+            # current = monthsProfits[idx][1]
+            # next = monthsProfits[idx + 1][1]
+            # ['feb-2012', 12312] - ['mar-2012', 1231]
+            current = monthsProfits[idx]
+            next = monthsProfits[idx + 1]
+            monthDifference = [next[0], int(next[1] - current[1])] 
+            monthDifferences.append(monthDifference)
+            # monthDifferences.append(int(next - current))
+
+    # print(monthDifferences)
 
     # gives a list of all of the differences (changes) between the months
     # now, find average by adding all of the numbers in the list
     # and dividing by the length of the list
     avgMonthlyDifferencesTotal = 0
     for monthDifference in monthDifferences:
-        avgMonthlyDifferencesTotal += monthDifference
-
+        avgMonthlyDifferencesTotal += monthDifference[1]
 
 
     print("Financial Analysis")
@@ -60,10 +68,21 @@ with open(csvpath, newline="") as csvfile:
 
     avgChange = avgMonthlyDifferencesTotal / len(monthDifferences)
     print(f"Average Change: ${round(avgChange, 2)}")
-    
-    # calculate the average of the changes in profit/losses
-    #bankAverage = (total / count)
-    #print(bankAverage)
+
+
+    max_month = monthDifferences[0]
+    min_month = monthDifferences[0]
+
+    # ['month',difference]
+    for month_difference in monthDifferences:
+        if (month_difference[1] > max_month[1]):
+            max_month = month_difference
+
+        if (month_difference[1] < min_month[1]):
+            min_month = month_difference
+        
+    print(f"Greatest Increase in Profits: {str(max_month[0])} (${str(max_month[1])})")
+    print(f"Greatest Decrease in Profits: {str(min_month[0])} (${str(min_month[1])})")
 
 
 
